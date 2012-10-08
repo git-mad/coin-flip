98be83584a178ca59b09cb0334c6ef86f68851f8
jzhang300 <james.zhang75024@gmail.com>
2012-10-08 18:37:10 -0400

    Animation for ResultActivity.java

--- src/coinFlipV1/gitmad/app/ResultActivity.java
+++ src/coinFlipV1/gitmad/app/ResultActivity.java
@@ -8,8 +8,14 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.animation.Animation;
+import android.view.animation.Animation.AnimationListener;
+import android.view.animation.AnimationSet;
+import android.view.animation.RotateAnimation;
+import android.view.animation.ScaleAnimation;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.ViewAnimator;
 
 public class ResultActivity extends Activity implements OnClickListener {
 	private String result = "not set";
@@ -22,22 +28,25 @@ public class ResultActivity extends Activity implements OnClickListener {
 		
 		setResult(flipCoin());
 		
+		Log.d("Demo", getResult());
+		
+		int images[] = {R.drawable.heads, R.drawable.tails};
+		ImageView resultImageView = (ImageView) this.findViewById(R.id.result_value_image);
+		resultImageView.setBackgroundResource(images[0]);
+		
+		//TextView text = (TextView) this.findViewById(R.id.result_value_label);
+		//text.setText(getResult());
+		
 		if (getResult() == "heads")
 		{
 			resultImage = R.drawable.heads;
+			flipAnimate(resultImageView, images, 0, 9, true);
 		}
 		else
 		{
 			resultImage = R.drawable.tails;
+			flipAnimate(resultImageView, images, 0, 7, true);
 		}
-		
-		Log.d("Demo", getResult());
-		
-		ImageView resultImageView = (ImageView) this.findViewById(R.id.result_value_image);
-		resultImageView.setBackgroundResource(resultImage);
-		
-		TextView resultView = (TextView) this.findViewById(R.id.result_value_label);
-		resultView.setText(this.getResult());
 
 		View flipCoinButton = findViewById(R.id.back_to_menu_button);
 		flipCoinButton.setOnClickListener(this);
@@ -62,6 +71,54 @@ public class ResultActivity extends Activity implements OnClickListener {
 			result = "tails";
 
 		return result;
+	}
+	
+	private void flipAnimate(final ImageView imageView, final int images[], final int imageIndex, final int iterations, final boolean isShrunk) {
+		
+		if (iterations > 0) {
+		
+			Animation curAnim = null;
+			ScaleAnimation shrink = new ScaleAnimation(1, 0, 1, 1, Animation.RELATIVE_TO_SELF, (float) 0.5, Animation.RELATIVE_TO_SELF, (float) .5);
+			ScaleAnimation expand = new ScaleAnimation(0, 1, 1, 1, Animation.RELATIVE_TO_SELF, (float) 0.5, Animation.RELATIVE_TO_SELF, (float) .5);
+			
+			if (isShrunk)
+			{
+				curAnim = expand;
+				imageView.setBackgroundResource(images[imageIndex]);
+			}
+			else{
+				curAnim = shrink;
+			}
+			
+			curAnim.setDuration(300);
+			
+			imageView.setAnimation(curAnim);
+			
+			curAnim.setAnimationListener(new AnimationListener() {
+				public void onAnimationEnd(Animation animation) {
+					int newIndex = imageIndex;
+					if (isShrunk)
+					{
+						if (imageIndex == 1)
+						{
+							newIndex = 0;
+						}
+						else
+						{
+							newIndex = 1;
+						}
+					}
+					
+					flipAnimate(imageView, images, newIndex, iterations-1, !isShrunk);
+		        }
+		        public void onAnimationRepeat(Animation animation) {
+		            // TODO Auto-generated method stub
+		        }
+		        public void onAnimationStart(Animation animation) {
+		            // TODO Auto-generated method stub
+		        }
+			});
+		}
 	}
 
 	@Override
